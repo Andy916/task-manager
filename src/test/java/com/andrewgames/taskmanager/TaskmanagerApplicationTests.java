44b6e364c8940966c3fdb00467f466925ad36e74
Andy Hattabaugh <andyhattabaugh@gmail.com>
2025-11-23 22:56:49 -0500

    Initial commit with starter endpoints

---
+++ src/test/java/com/andrewgames/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewgames.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
